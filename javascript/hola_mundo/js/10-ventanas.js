3c4ebd5571966299896d05fad20ba1ee5e9a040b
Mike <lanselot777hailander@gmail.com>
2018-06-20 10:26:29 -0400

    bucles

---
+++ javascript/hola_mundo/js/10-ventanas.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Alerta
+alert("Esta es mi alerta!!!");
+alert("Este es mi texto");
+
+// Confirmacion
+var mi_resultado = confirm("¿Estas seguro de querer continuar?");
+console.log(mi_resultado);
+
+// Ingreso datos
+var resultado = prompt("¿Que edad tienes?", 18);
+console.log(typeof resultado);
