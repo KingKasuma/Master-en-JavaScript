c8754652aede5e8448f1eb87996ff0a5c1c169e5
Mike <lanselot777hailander@gmail.com>
2018-07-06 11:13:18 -0400

    Eventos

---
+++ javascript/hola_mundo/js/30-eventos.js
@@ -0,0 +1,65 @@
+'use strict'
+
+// Eventos del raton
+
+window.addEventListener('load', function(){
+  var boton = document.querySelector("#boton");
+
+  function cambiarColor(){
+    console.log("Me has dado click");
+    var bg = boton.style.background;
+
+    if (bg == "green none repeat scroll 0% 0%") {
+      boton.style.background = "red";
+    }else{
+      boton.style.background = "green";
+    }
+
+    boton.style.padding = "10px";
+    boton.style.border = "1px solid #ccc";
+    console.log(bg == "green none repeat scroll 0% 0%");
+    return true;
+  }
+
+  // Click
+  boton.addEventListener('click', function(){
+    cambiarColor();
+  });
+
+  // Mouse over
+  boton.addEventListener('mouseover', function(){
+    boton.style.background = "yellow";
+  });
+
+  // Mouseout
+  boton.addEventListener('mouseout', function(){
+    boton.style.background = "#ccc";
+  });
+
+
+   //Focus
+   var input = document.querySelector("#campo_nombre")
+   input.addEventListener('focus', function(){
+     console.log("[focus] Estas dentro del input");
+   });
+
+   // Blur
+   input.addEventListener('blur', function(){
+     console.log("[blur] Estas fuera del input");
+   });
+
+  // Keydown
+  input.addEventListener('keydown', function(event){
+    console.log("[keydown] Pulsando esta tecla", String.fromCharCode(event.keyCode));
+  });
+
+  // Keypress
+  input.addEventListener('keypress', function(event){
+    console.log("[keypress] Tecla presionada", String.fromCharCode(event.keyCode));
+  });
+
+  // Keyup
+  input.addEventListener('keyup', function(event){
+    console.log("[keyup] Tecla soltada", String.fromCharCode(event.keyCode));
+  });
+});
